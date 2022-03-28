4f274904eb460492deb9c6ab9c40ba31d10880f2
Omar Peña <opena854@gmail.com>
2022-03-28 00:14:43 -0400

    more readable

--- index.js
+++ index.js
@@ -7,69 +7,65 @@ const app = express()
 
 const config = {
   servicePort: env.PORT || 3000,
-  externalCacheService: env.CACHE_SERVICE || undefined,
+  cacheService: env.CACHE_SERVICE || `http://localhost:${env.PORT || 3000}/cache`,
   launchPriceService: env.PRICE_SERVICE || true
 }
 
-const asBool = value => typeof value === "string" ? (value.toLowerCase() === "true" || value === "1") : !!value
-
-const getExternalPrice = async numberPlate => {
-  console.log(`getExternalPrice("${numberPlate}") query`);
-  const secondsDelay = 10
-  const randomPrice = Math.floor((1 + Math.random()) * 100) * 100
-  return new Promise(r => setTimeout(() => {
-    console.log(`getExternalPrice("${numberPlate}") reponse`);
-    r({plate: numberPlate, price: randomPrice})
-  }, secondsDelay * 1000));
-}
-
-if (!config.externalCacheService) { //serves it's own cache services since it's not defined in config.
-  app.put('/cache', ({ query: {plate, price} }, res) => set(plate, price).then( () => res.send().end()))
-
+if (!config.externalCacheService) { //serves it's own cache service since it's not defined in config.
   app.get('/cache', ({ query: {plate, skipCache} }, res) => read(plate, asBool(skipCache)).then( result => res.send(result).end()))
+  app.put('/cache', ({ query: {plate, price} }, res) => set(plate, price).then( () => res.send().end()))
 }
 
 if (config.launchPriceService) {
-  app.get("/price", ({ query: {plate, skipCache = true} }, response) => {
-    console.log(`getPrice("${plate}", ${asBool(skipCache)}) call`);
-    const cacheService = new URL(
-      config.externalCacheService || "http://localhost:3000/cache"
-    );
-
-    cacheService.searchParams.set("plate", plate);
-    cacheService.searchParams.set("skipCache", asBool(skipCache));
-
-    let output = "";
-
-    http.request(cacheService, { method: "GET" }, res => {
-      res.setEncoding("utf8");
-      res.on("data", (chunk) => (output += chunk));
-      res.on("end", () => {
-        let obj = JSON.parse(output);
-
-        if (obj?.status === "found") {
-          console.log(`getPrice("${plate}", ${asBool(skipCache)}) response: ${Number(obj?.value)}`);
-          response.send({ plate: plate, price: Number(obj?.value) });
-        }
-        else if (obj?.status === "not found") {
-          getExternalPrice(plate).then((result) => {
-            cacheService.searchParams.delete("skipCache");
-            cacheService.searchParams.set("price", result.price);
-
-            http.request(cacheService, { method: "PUT" }, () => {
-              //console.log("cache refreshed");
-              console.log(`getPrice("${plate}", ${asBool(skipCache)}) response: ${result.price}`);
-              response.send(result);
-            }).end();
-          });
-        }
-      });
-    }).end();
-  });
+  app.get("/price", ({ query: {plate, skipCache = true} }, response) => getPrice(plate, skipCache).then( result => response.send(result) ));
 }
 
 app.listen(config.servicePort, () => {
   console.log(`Service listening on port ${config.servicePort}`);
 });
 
+const asBool = value => typeof value === "string" ? (value.toLowerCase() === "true" || value === "1") : !!value
 
+const getPrice = (plate, skipCache = true) =>
+  callService(
+    config.cacheService,
+    { method: "GET" },
+    { plate, skipCache: asBool(skipCache) }
+  ).then(({ status, value }) =>
+    status === "found"
+      ? { plate, price: Number(value) }
+      : getExternalPrice(plate).then((result) =>
+          callService(
+            config.cacheService,
+            { method: "PUT" },
+            { plate, price: result.price }
+          ).then(() => result)
+        )
+  );
+
+const callService = (url, options= {}, query = {} ) => new Promise( r => {
+  const urlObj = (url instanceof URL) ? url : new URL(url)
+  Object.entries(query).forEach( (keyval) => urlObj.searchParams.append(...keyval))
+
+  let output = ""
+  http.request(urlObj, options, res => {
+    if (res.headers['content-length'] > 0) {
+      res.setEncoding("utf8");
+      res.on("data", (chunk) => (output += chunk));
+      res.on("end", () => r(JSON.parse(output)))
+    } else {
+      r(undefined)
+    }
+    
+  }).end()
+})
+  
+const getExternalPrice = async numberPlate => {
+  //console.log(`getExternalPrice("${numberPlate}") query`);
+  const secondsDelay = 7
+  const randomPrice = Math.floor((1 + Math.random()) * 100) * 100
+  return new Promise(r => setTimeout(() => {
+    //console.log(`getExternalPrice("${numberPlate}") reponse`);
+    r({plate: numberPlate, price: randomPrice})
+  }, secondsDelay * 1000));
+}
--- src/cache.js
+++ src/cache.js
@@ -2,30 +2,26 @@
 export const cache = {}
 export const precache = {}
 
+const TIMEOUT = 120
+
 const sleep = async ms => new Promise(r => setTimeout(r, ms));
 
-export const read = async (key, skipCache = true) => {
-    //console.log(cache, precache)
-    return new Promise( resolve => {
+export const read = async (key, skipCache = true) => new Promise( resolve => {
         
-        if (!skipCache && !!cache[key]) resolve ({status: "found", value: cache[key]})
+        if (!skipCache && !!cache[key]) resolve ({status: "found", value: cache[key].value})
         else if (!!precache[key]) precache[key].then( (result) => resolve({status: "found", value: result}) )
         else {
-            //console.log("it's gonna be read and should be precached.")
-            const oldcachestatus = cache[key] //saving oldstatus for scenario where cache and precache exits
-            precache[key] = new Promise ( async (resolve, reject) => {
-                let tic = 60
+            const oldCacheStatus = cache[key] //saving oldstatus for scenario where cache and precache exits
+            precache[key] = new Promise ( async (precacheResolve, reject) => {
+                let tic = TIMEOUT * 2
                 
-                while (tic--) {
-                    //console.log("waiting for cache... ", tic)
-                    if (cache[key] !== oldcachestatus) {
-                        //console.log("found price", cache[key])
-                        resolve (cache[key]);
+                while (tic--)
+                    if (cache[key] !== oldCacheStatus) {
+                        precacheResolve (cache[key].value);
                         delete precache[key]
                         return;
                     } 
-                    await sleep(1000);   
-                }
+                    else await sleep(500);   
                 reject ("timeout");
 
             }).catch( console.log )
@@ -33,7 +29,6 @@ export const read = async (key, skipCache = true) => {
             resolve({status: "not found"})
         }
     })
-}
 
-export const set = async (key, value) => cache[key] = value
+export const set = async (key, value) => cache[key] = { value, ts: new Date() } 
 
